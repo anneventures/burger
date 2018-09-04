f48f8306e97c5f99800391f9c111907c8d635665
Annecodes <info.annesimon@gmail.com>
2018-09-04 16:57:29 -0400

    finalized file

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var router = require('./controllers/burgers_controller.js');
+app.use('/', router);
+
+app.listen(PORT, function() 
+{
+  console.log("App listening on PORT " + PORT);
+});
