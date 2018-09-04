2503a8b065b513e2376e99ee29f3ff19499cd646
Annecodes <info.annesimon@gmail.com>
2018-09-04 11:03:02 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Chicken', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Beef', false);
